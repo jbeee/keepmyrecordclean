f9c4958b457b3143245fcbda056b425c265104bc
unknown <jana@jbee.io>
2014-04-16 14:16:53 -0700

    initial commit, just file structure and some compressed dependencies

---
+++ js/globals.js
@@ -0,0 +1,75 @@
+var newAccount = {
+    "_id": -1,
+    "dateCreated": "",
+    "username": "",
+    "email": "",
+    "mailinglist": "",
+    "fbid": "",
+    "gplus": "",
+    "twitter": "",
+    "confirmed": "",
+    "password": "",
+    "idents": {
+        "fname": "",
+        "mi": "",
+        "lname": "",
+        "DOB": "",
+        "DL": "",
+        "DTYPE": "",
+        "gender": "",
+        "address": {
+            "street": "",
+            "apartment": "",
+            "city": "",
+            "state": "",
+            "zip": ""
+        },
+        "phones": {
+            "hphone": "",
+            "aphone": "",
+            "fax": ""
+        }
+    },
+    "cases": [
+        {
+            "_id": "000",
+            "client": "",
+            "type": "",
+            "date_recieved": "",
+            "place": {
+                "city": "",
+                "state": "",
+                "county": ""
+            },
+            "courtname": "",
+            "infractions": "",
+            "accident": "",
+            "dui": "",
+            "courtdate": "",
+            "lawyer": "",
+            "confirmed": "",
+            "paid": "",
+            "won": false
+        },
+        {
+            "_id": "001",
+            "client": "",
+            "type": "",
+            "date_recieved": "",
+            "place": {
+                "city": "",
+                "state": "",
+                "county": ""
+            },
+            "courtname": "",
+            "infractions": "",
+            "accident": "",
+            "dui": "",
+            "courtdate": "",
+            "lawyer": "",
+            "confirmed": "",
+            "paid": "",
+            "won": false
+        }
+    ]
+}
